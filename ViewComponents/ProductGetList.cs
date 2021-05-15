d5b486cf84633f25b50bad2ff66c9f3aa387b1f1
Sedat Bilece <seda1tbilece@gmail.com>
2021-05-15 14:01:31 +0300

    ana menüde foodların getirilmesi

---
+++ ViewComponents/ProductGetList.cs
@@ -0,0 +1,23 @@
+﻿using CoreAndFood.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreAndFood.ViewComponents
+{
+    public class ProductGetList :ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            FoodRepository fd = new FoodRepository();
+
+            var fdlist = fd.TList();
+            return View(fdlist);
+        }
+
+       
+    }
+}
